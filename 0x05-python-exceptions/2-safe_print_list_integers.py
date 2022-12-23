9acd53ae9f633d38c40b1e91690501ae8094b6cd
Quzeem <Quzeemoddusanwo@gmail.com>
2022-12-23 14:06:49 +0000

    All tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+ File  16 lines (14 sloc)  326 Bytes
+
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            count += 1
+
+    print()
+    return (count)
